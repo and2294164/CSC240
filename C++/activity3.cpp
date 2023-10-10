686d460b5d16c260957d5cb324b4c72fc697f9f2
and2294164 <and2294164@maricopa.edu>
2023-10-10 15:36:30 -0700

    final commit

--- C++/activity3.cpp
+++ C++/activity3.cpp
@@ -13,11 +13,30 @@ class Grades {
         iter = record.begin();
     } 
 
+    
     void add(int x) {
 
+        record.push_back(x);
+    }
+
+    void add(vector<int> v) {
+
+        for(iter = v.begin(); iter != v.end(); iter++) {
+        
+            record.push_back(*iter);
+        }
     }
 
     void print() {
+
+        double total;
+
+        for(iter = record.begin(); iter != record.end(); iter++) {
+
+            total += *iter;
+            cout << "record[" << distance(record.begin(), iter) << "] = " << *iter << endl;
+        }
+        cout << "average = " << total / record.size() << endl;
 
     }
 
@@ -26,6 +45,20 @@ class Grades {
 
 int main() {
 
+    vector<int> scores{81, 90, 79, 85};
+
+
+    Grades reportCard(scores);
+    reportCard.print();
+
+    cout << endl << "after using vector.add(int x)" << endl;
+    reportCard.add(100);
+    reportCard.print();
+
+    cout <<endl << "after using vector.add(vector<int> v)" << endl;
+    vector<int> moreScores{80, 85, 90, 95, 100};
+    reportCard.add(moreScores);
+    reportCard.print();
 
 
 
